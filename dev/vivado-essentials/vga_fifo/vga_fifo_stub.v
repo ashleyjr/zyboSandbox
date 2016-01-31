aedec5de565330a6d0f9b5f692fac21efe961dd7
Ashley J. Robinson <ashley181291@gmail.com>
2016-01-31 17:52:06 +0000

    move vivado stuff into dev and files from the zybo os in to zybo

---
+++ dev/vivado-essentials/vga_fifo/vga_fifo_stub.v
@@ -0,0 +1,28 @@
+// Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2015.4 (lin64) Build 1412921 Wed Nov 18 09:44:32 MST 2015
+// Date        : Sun Jan 31 16:42:42 2016
+// Host        : Heidi running 64-bit Ubuntu 14.04.3 LTS
+// Command     : write_verilog -force -mode synth_stub /home/ashley/zyboSandbox/vivado-essentials/vga_fifo/vga_fifo_stub.v
+// Design      : vga_fifo
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_0_1,Vivado 2015.4" *)
+module vga_fifo(rst, wr_clk, rd_clk, din, wr_en, rd_en, dout, full, empty, prog_full)
+/* synthesis syn_black_box black_box_pad_pin="rst,wr_clk,rd_clk,din[35:0],wr_en,rd_en,dout[35:0],full,empty,prog_full" */;
+  input rst;
+  input wr_clk;
+  input rd_clk;
+  input [35:0]din;
+  input wr_en;
+  input rd_en;
+  output [35:0]dout;
+  output full;
+  output empty;
+  output prog_full;
+endmodule
---
+++ zybo/mount_sd.sh
@@ -0,0 +1,3 @@
+rmdir /mnt/sd
+mkdir /mnt/sd
+mount /dev/mmcblk0p1 /mnt/sd
